d1463fb0caedc67e0180a939c955d628434cc1fb
Erik Novales <enovales@codehitmen.com>
2016-10-25 09:25:48 -0700

    added negative tests for conversions

--- tests/LANGIDMappings.Tests/Tests.fs
+++ tests/LANGIDMappings.Tests/Tests.fs
@@ -18,9 +18,17 @@ let ``langIdSubLanguage returns the correct sublanguage from a LANGID`` () =
     Assert.AreEqual(0x01us, LANGIDMappings.langidSubLanguage(langidEnglishUs))
 
 [<Test>]
+let ``converting a LANGID to ISO-639-1 without a mapping returns None`` () = 
+    Assert.AreEqual(None, LANGIDMappings.LANGIDToIso639Dash1(0xFFFFus))
+
+[<Test>]
 let ``can convert a LANGID mapping to an ISO-639-1 code`` () =
   let isoCodeOpt = LANGIDMappings.LANGIDToIso639Dash1(langidEnglishUs)
   Assert.AreEqual(Some("en-US"), isoCodeOpt)
+
+[<Test>]
+let ``converting an ISO-639-1 code to a LANGID without a mapping returns None`` () = 
+    Assert.AreEqual(None, LANGIDMappings.Iso639Dash1ToLANGID("zz-ZZ"))
 
 [<Test>]
 let ``can convert an ISO-639-1 code to a LANGID mapping`` () = 
