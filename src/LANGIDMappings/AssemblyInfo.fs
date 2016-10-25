03a64de6a35c57913b4de499627e8fca79336a53
Erik Novales <enovales@codehitmen.com>
2016-10-24 22:42:32 -0700

    Bump version to 0.1.1

--- src/LANGIDMappings/AssemblyInfo.fs
+++ src/LANGIDMappings/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("LANGIDMappings")>]
 [<assembly: AssemblyProductAttribute("LANGIDMappings")>]
 [<assembly: AssemblyDescriptionAttribute("Mappings between Windows LANGIDs and other standards")>]
-[<assembly: AssemblyVersionAttribute("0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "LANGIDMappings"
     let [<Literal>] AssemblyProduct = "LANGIDMappings"
     let [<Literal>] AssemblyDescription = "Mappings between Windows LANGIDs and other standards"
-    let [<Literal>] AssemblyVersion = "0.1"
-    let [<Literal>] AssemblyFileVersion = "0.1"
+    let [<Literal>] AssemblyVersion = "0.1.1"
+    let [<Literal>] AssemblyFileVersion = "0.1.1"
